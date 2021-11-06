c595122ee5b74f892efe9f12fc03a22813e40a20
Mickaël BANNHOLTZER <mbannholtzer@numerique-educatif.fr>
2021-11-06 16:30:49 +0100

    ajout classe shepherd

---
+++ Solution/Project/shepherd.cpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "shepherd.h"
+
+shepherd::shepherd() {}
+
+shepherd::~shepherd(){}
+
+//virtual void move
---
+++ Solution/Project/shepherd.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+class shepherd {
+
+public:
+	shepherd();
+	~shepherd();
+
+	//méthode
+	virtual void  move() = 0 ;
+};
